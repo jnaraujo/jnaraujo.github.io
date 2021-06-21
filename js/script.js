15b1527074a24184ef5c6b7d2b2f0febdc4f400e
Jonatas A <jonatasaraujofx@gmail.com>
2021-06-21 00:55:57 -0300

    add some effects

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return elemBottom <= docViewBottom && elemTop >= docViewTop;
+    }
+
+    // $("#portfolio .row .item").each(function () {
+    //     if (isScrolledIntoView(this) === true) {
+    //         if (!$(this).hasClass("animate__animated animate__fadeInUp")) {
+    //             $(this).removeClass("hidden")
+    //             $(this).addClass("animate__animated animate__fadeInUp");
+    //         }
+    //     }
+    // });
+    
+    // If element is scrolled into view, fade it in
+    $(window).scroll(function () {
+        $("#portfolio .row .item").each(function () {
+            if (isScrolledIntoView(this) === true) {
+                if (!$(this).hasClass("animate__animated animate__fadeInUp")) {
+                    $(this).removeClass("hidden")
+                    $(this).addClass("animate__animated animate__fadeInUp");
+                }
+            }
+        });
+    });
+});
