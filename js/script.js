1436f34149e7fbb1d5c61f39411310e3062053a7
Jonatas A <jonatasaraujofx@gmail.com>
2021-06-21 01:10:08 -0300

    add some effects 2

--- js/script.js
+++ js/script.js
@@ -17,11 +17,20 @@ $(document).ready(function () {
     //         }
     //     }
     // });
+
+    $("#portfolio .row .item").each(function () {
+        if (isScrolledIntoView($("#portfolio")) === true) {
+            if (!$(this).hasClass("animate__animated animate__fadeInUp")) {
+                $(this).removeClass("hidden")
+                $(this).addClass("animate__animated animate__fadeInUp");
+            }
+        }
+    });
     
     // If element is scrolled into view, fade it in
     $(window).scroll(function () {
         $("#portfolio .row .item").each(function () {
-            if (isScrolledIntoView(this) === true) {
+            if (isScrolledIntoView($("#portfolio")) === true) {
                 if (!$(this).hasClass("animate__animated animate__fadeInUp")) {
                     $(this).removeClass("hidden")
                     $(this).addClass("animate__animated animate__fadeInUp");
